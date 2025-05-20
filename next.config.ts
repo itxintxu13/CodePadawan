8bcb83b40f9421769c32ab3d067a6ccd1e82ad90
ARP-10 <alejandrarodriguezp97@gmail.com>
2025-05-20 21:40:52 +0200

    cambios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // aquí puedes añadir más opciones si quieres
 };
 
 export default nextConfig;
